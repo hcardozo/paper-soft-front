7c54f0391270b2bd16e24b9cbebf5e57df5eb6f6
Clinares <camilolinares.981@gmail.com>
2023-11-09 22:34:35 -0500

    Se realiza front del modulo de login

--- paperSoft/src/app/modules/login/login-routing.module.ts
+++ paperSoft/src/app/modules/login/login-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }, 
+  {
+    path: 'registroUsuario',
+    component: RegistroUsuarioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- paperSoft/src/app/modules/login/login.module.ts
+++ paperSoft/src/app/modules/login/login.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { HomeComponent } from './components/home/home.component';
-
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
